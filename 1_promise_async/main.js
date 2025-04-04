71d4e13266cd6def40219aa99a02f98210706b18
Buttyan Peter Samuel <buttyan.peter@BOLYAI.LOCAL>
2025-04-04 12:40:29 +0200

    es igy mukodik az internet ahh

--- 1_promise_async/main.js
+++ 1_promise_async/main.js
@@ -1,3 +1,5 @@
+
+
 const buyApple=(applenumber)=>{
     if(applenumber<5){
         return Promise.resolve('Van eleg alma');
@@ -20,3 +22,98 @@ buyApple(5).then((value)=>{
 }).catch((error)=>{
     console.log(error)
 })
+
+
+
+const buyApple2=(applenumber=>{
+    return new Promise((resolve,reject) =>{
+        if(applenumber<5){
+            resolve('Van eleg alma sigma')
+        }
+        else{
+            reject('Nincs eleg alma sigma')
+        }
+    })
+})
+
+buyApple2(3).then((respektkrokodilo)=>{
+    console.log(respektkrokodilo);
+}).catch((error)=>{
+    console.log(error)
+})
+
+buyApple2(5).then((respektkrokodilo)=>{
+    console.log(respektkrokodilo);
+}).catch((error)=>{
+    console.log(error)
+})
+
+setTimeout(()=>{
+},0)
+
+const buyApple3=(applenumber=>{
+    return new Promise((resolve,reject) =>{
+        if(applenumber<5){
+            setTimeout(()=>{
+                resolve('Van eleg alma respekt') 
+            },2000)
+        }
+        else{
+            setTimeout(()=>{
+                reject('Nincs eleg alma fuuj') 
+            },3000)
+        }
+    })
+})
+
+buyApple3(4).then((skibidi)=>{
+    console.log(skibidi);
+}).catch((error)=>{
+    console.log(error)
+})
+
+const res2 = buyApple3(5);
+console.log(res2)
+
+
+class Service{
+    #data
+    constructor(){
+        this.#data = people;
+    }
+    init(){
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                resolve(this.#data);
+            },3000)
+        })
+     
+    }
+}
+
+class dataviewController{
+    #div
+    constructor(){
+        this.#div = document.createElement('div');
+        this.#div.textContent= 'Loading';
+        document.body.appendChild(this.#div)
+    }
+
+    setContent(array){
+        this.#div.innerHTML = "";
+        for(const element of array){
+           const div = document.createElement('div')
+           div.textContent = element.name;
+           this.#div.appendChild(div)
+        }
+    }
+
+    
+}
+
+const ser = new Service;
+const view = new dataviewController;
+ser.init().then((value)=>{
+    view.setContent(value)
+})
+
